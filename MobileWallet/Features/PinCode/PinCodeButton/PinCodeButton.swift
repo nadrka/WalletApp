bdb7d15d5ae47374418f63014d4f192635159b4e
Karol Nadratowski <user@Users-MacBook-Pro.local>
2021-04-23 08:40:12 +0200

    Revert "WIP"
    This reverts commit e1389014f86517592cacdd8ec04af708f30b9206.

--- MobileWallet/Features/PinCode/PinCodeButton/PinCodeButton.swift
+++ MobileWallet/Features/PinCode/PinCodeButton/PinCodeButton.swift
@@ -32,7 +32,7 @@ struct PinCodeButton: View {
 struct PinCodeButton_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            PinCodeButton(viewModel: PinCodeButtonViewModel(type: .value("8")))
+            PinCodeButton(viewModel: PinCodeButtonViewModel(type: .value("9")))
                 .frame(width: 100, height: 100)
             
             PinCodeButton(viewModel: PinCodeButtonViewModel(type: .backSpace))
